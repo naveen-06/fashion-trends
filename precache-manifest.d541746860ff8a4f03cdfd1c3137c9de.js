a3fb604706fb392de90a5179f294f043757ae79b
naveen-06 <70632802+naveen-06@users.noreply.github.com>
2020-09-05 01:22:33 -0400

    fashion trends application
    e-commerce application where things can be wish-listed and can be purchased

---
+++ precache-manifest.d541746860ff8a4f03cdfd1c3137c9de.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "198804c3495b40449f817fa78163ef49",
+    "url": "/fashion-trends/index.html"
+  },
+  {
+    "revision": "4ab6fdac3ab6ce65d879",
+    "url": "/fashion-trends/static/css/main.f1063e3d.chunk.css"
+  },
+  {
+    "revision": "772329e808f780e89628",
+    "url": "/fashion-trends/static/js/2.cc9f4fd2.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/fashion-trends/static/js/2.cc9f4fd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ed70779a703fd1479f8",
+    "url": "/fashion-trends/static/js/3.24b3fcca.chunk.js"
+  },
+  {
+    "revision": "03b46cb6235659766343",
+    "url": "/fashion-trends/static/js/4.b1776236.chunk.js"
+  },
+  {
+    "revision": "cfd2c41a9fdb3556e575",
+    "url": "/fashion-trends/static/js/5.b037074b.chunk.js"
+  },
+  {
+    "revision": "4ab6fdac3ab6ce65d879",
+    "url": "/fashion-trends/static/js/main.ef0f1ee0.chunk.js"
+  },
+  {
+    "revision": "6b8c9cd1999a11e6e3ed",
+    "url": "/fashion-trends/static/js/runtime-main.1d962f58.js"
+  },
+  {
+    "revision": "bc857127d0d20bf6924421a991b73790",
+    "url": "/fashion-trends/static/media/trends.bc857127.jpeg"
+  }
+]);
